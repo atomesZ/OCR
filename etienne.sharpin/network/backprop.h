977c84fea58fdf4a8041c6f9ccac6959a7b7a12d
Blankenberg Eliot <36482893+EliotBlankenberg@users.noreply.github.com>
2018-10-21 20:48:04 +0200

    Add files via upload

---
+++ etienne.sharpin/network/backprop.h
@@ -0,0 +1,26 @@
+#ifndef OCR_BACKPROP_H
+#define OCR_BACKPROP_H
+
+#include "network.h"
+
+double cost(double out,double expected);
+
+double cost_prime_out(double out,double expected);
+
+double out_prime_sum(double out);
+
+/*double sum_prime_weight(int n)*/
+
+//double cost_prime_weight(double out,double expected,double n)
+double cost_prime_weight(double out,double n,double a);
+
+double cost_prime_bias(double out,double expected);
+
+double cost_prime_sum(double out,double expected,double weight);
+
+//double total_cost(Matrix out,Matrix in)
+double total_cost(double out,double expected);
+
+int backprop(Network N,double out,double expected);
+
+#endif //OCR_BACKPROP_H
