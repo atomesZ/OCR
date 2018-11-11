8f876495215c9674add50e1497992379fdde6e0a
etienne.sharpin <etienne.sharpin@epita.fr>
2018-11-11 19:51:52 +0100

    débuts tests XOR
    Signed-off-by: etienne.sharpin <etienne.sharpin@epita.fr>

--- network/main.c
+++ network/main.c
@@ -6,8 +6,8 @@ int main()
 	int array[] = {2,3,1};
 	int *sizes = array;
 	Network net = initNetwork(sizes);
-	net.n_outputs[0] = 0;
-	net.n_outputs[1] = 0;
+	net.n_outputs[0] = 1;
+	net.n_outputs[1] = 1;
 	printNet(net);
 	feedforward(net, net.n_outputs);
 	printf("\n");
@@ -15,9 +15,26 @@ int main()
 	printf("\n");
 	printf("final output: %lf ", net.n_outputs[net.num_neurons - 1]);
 
+	srand(time(NULL));
+	int a;
+	int b;
 	for(int i = 0; i < 300; ++i)
 	{
-		backprop(net, net.n_outputs[net.num_neurons - 1], 0);
+		a = rand();
+		b = rand();
+		if(a < RAND_MAX / 2)
+			a = 0;
+		else
+			a = 1;
+
+		if(b < RAND_MAX / 2)
+			b = 0;
+		else
+			b = 1;
+		
+		net.n_outputs[0] = a;
+		net.n_outputs[1] = b;
+		backprop(net, net.n_outputs[net.num_neurons - 1], a + b == 1);
 		feedforward(net, net.n_outputs);
 		printNet(net);
 	}
