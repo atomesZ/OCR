47c6f8b4722fe2124ad55b9eb0b46ccddd3e52c9
Blankenberg Eliot <36482893+EliotBlankenberg@users.noreply.github.com>
2018-11-11 23:50:19 +0100

    Add files via upload

--- network/backprop.c
+++ network/backprop.c
@@ -1,10 +1,15 @@
 #include "backprop.h"
+#include "matrix.h"
 #include "network.h"
 #include <math.h>
 
 double cost(double out,double expected)
 {
-    return (out - expected) * (out - expected);
+    if((out - expected) < 0)
+        return (out - expected) * (out - expected);
+    else
+        return (out - expected);
+    /*return (out - expected) * (out - expected);*/
 }
 
 double cost_prime_out(double out,double expected)
@@ -50,6 +55,8 @@ double total_cost(double out,double expected)         //XOR
         total_cost += cost(out,expected);                         //XOR
     }
 
+    printf("%d",total_cost);
+
     return total_cost;
 }
 
@@ -78,21 +85,21 @@ void backprop(Network N,double out,double expected)                    // XOR
     }*/
                                                                                                 //XOR :
 
-    double a = cost_prime_sum(out,expected,N.weights[6]) + cost_prime_sum(out,expected,N.weights[5]) + cost_prime_sum(out,expected,N.weights[4]);
+    double a = cost_prime_sum(out,expected,N.weights[5]) + cost_prime_sum(out,expected,N.weights[4]) + cost_prime_sum(out,expected,N.weights[3]);
 
-    N.weights[8] = N.weights[8] - 1*cost_prime_weight(out,N.n_outputs[2],cost_prime_out(out,expected));
-    N.weights[7] = N.weights[7] - 1*cost_prime_weight(out,N.n_outputs[2],cost_prime_out(out,expected));
-    N.weights[6] = N.weights[6] - 1*cost_prime_weight(out,N.n_outputs[2],cost_prime_out(out,expected));
+    N.weights[8] = N.weights[8] - 0.1*cost_prime_weight(out,N.n_outputs[2],cost_prime_out(out,expected));
+    N.weights[7] = N.weights[7] - 0.1*cost_prime_weight(out,N.n_outputs[2],cost_prime_out(out,expected));
+    N.weights[6] = N.weights[6] - 0.1*cost_prime_weight(out,N.n_outputs[2],cost_prime_out(out,expected));
 
-    N.biases[5] = N.biases[5] - 1*cost_prime_bias(out,expected);
-    N.biases[4] = N.biases[4] - 1*cost_prime_bias(out,expected);
+    N.biases[4] = N.biases[4] - 0.1*cost_prime_bias(out,expected);
+    N.biases[3] = N.biases[3] - 0.1*cost_prime_bias(out,expected);
 
-    N.weights[5] = N.weights[5] - 1*cost_prime_weight(out,N.n_outputs[1],a);
-    N.weights[4] = N.weights[4] - 1*cost_prime_weight(out,N.n_outputs[1],a);
-    N.weights[3] = N.weights[3] - 1*cost_prime_weight(out,N.n_outputs[1],a);
-    N.weights[2] = N.weights[2] - 1*cost_prime_weight(out,N.n_outputs[0],a);
-    N.weights[1] = N.weights[1] - 1*cost_prime_weight(out,N.n_outputs[0],a);
-    N.weights[0] = N.weights[0] - 1*cost_prime_weight(out,N.n_outputs[0],a);
+    N.weights[5] = N.weights[5] - 0.1*cost_prime_weight(out,N.n_outputs[1],a);
+    N.weights[4] = N.weights[4] - 0.1*cost_prime_weight(out,N.n_outputs[1],a);
+    N.weights[3] = N.weights[3] - 0.1*cost_prime_weight(out,N.n_outputs[1],a);
+    N.weights[2] = N.weights[2] - 0.1*cost_prime_weight(out,N.n_outputs[0],a);
+    N.weights[1] = N.weights[1] - 0.1*cost_prime_weight(out,N.n_outputs[0],a);
+    N.weights[0] = N.weights[0] - 0.1*cost_prime_weight(out,N.n_outputs[0],a);
 }
 
 
